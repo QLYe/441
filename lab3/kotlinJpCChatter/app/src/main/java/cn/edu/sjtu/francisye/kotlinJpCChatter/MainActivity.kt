386c6d3bd70b6da908a8a97a70332657f0f77110
Francis-Y-y <2775515729@qq.com>
2024-07-10 16:57:13 +0800

    jpcchatter_lab3

---
+++ lab3/kotlinJpCChatter/app/src/main/java/cn/edu/sjtu/francisye/kotlinJpCChatter/MainActivity.kt
@@ -0,0 +1,46 @@
+package cn.edu.sjtu.francisye.kotlinJpCChatter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cn.edu.sjtu.francisye.kotlinJpCChatter.ui.theme.KotlinJpCChatterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController, startDestination = "MainView") {
+                composable("MainView") {
+                    MainView(this@MainActivity, navController)
+                }
+                composable("PostView") {
+                    PostView(this@MainActivity, navController)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinJpCChatterTheme {
+        Greeting("Android")
+    }
+}
